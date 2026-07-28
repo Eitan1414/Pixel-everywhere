53279f9ece6354dd781279098c3885ff23cf87bf
Pixel Studio Plugins <affairegamer@gmail.com>
2026-07-29 01:59:19 +0200

    Exposer l’installation automatique au client

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -4,5 +4,11 @@ contextBridge.exposeInMainWorld("pixelDesktop", {
   getRuntime: () => ipcRenderer.invoke("pixel:get-runtime"),
   openExternal: (url) => ipcRenderer.invoke("pixel:open-external", url),
   apiRequest: (request) => ipcRenderer.invoke("pixel:api-request", request),
-  selectUpdateFile: (request) => ipcRenderer.invoke("pixel:select-update-file", request)
+  selectUpdateFile: (request) => ipcRenderer.invoke("pixel:select-update-file", request),
+  installUpdate: (request) => ipcRenderer.invoke("pixel:install-update", request),
+  onUpdateProgress: (callback) => {
+    const listener = (_event, payload) => callback(payload);
+    ipcRenderer.on("pixel:update-progress", listener);
+    return () => ipcRenderer.removeListener("pixel:update-progress", listener);
+  }
 });
