947cfd3ae22fcc7f2764febe0b280d3fd69fa7cf
Pixel Studio Plugins <affairegamer@gmail.com>
2026-07-31 01:21:53 +0200

    Désactiver l’installation automatique macOS

--- electron/bootstrap.cjs
+++ electron/bootstrap.cjs
@@ -3,7 +3,6 @@ const { app } = require("electron");
 require("./startup-guard.cjs");
 
 app.whenReady().then(() => {
-  require("./automatic-updater.cjs");
   require("./main.cjs");
 }).catch((error) => {
   console.error("PIXEL_BOOTSTRAP_ERROR", error);
